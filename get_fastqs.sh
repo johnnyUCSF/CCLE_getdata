9152ed3bb89c5ecc96b5758ec836946f7f037e18
Johnny Yu <johnny.yu@ucsf.edu>
2020-07-22 18:08:59 -0700

    Create get_fastqs.sh

---
+++ get_fastqs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#wget https://sra-download.ncbi.nlm.nih.gov/traces/sra77/SRR/008413/SRR8615533;
+wget https://sra-download.ncbi.nlm.nih.gov/traces/sra77/SRR/008413/SRR8615612;
+wget https://sra-download.ncbi.nlm.nih.gov/traces/sra77/SRR/008414/SRR8615980;
+wget https://sra-download.ncbi.nlm.nih.gov/traces/sra77/SRR/008413/SRR8615603;
+wget https://sra-download.ncbi.nlm.nih.gov/traces/sra77/SRR/008413/SRR8615255;
+wget https://sra-download.ncbi.nlm.nih.gov/traces/sra77/SRR/008413/SRR8615813;
+wget https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-15/SRR8615536/SRR8615536.1;
+wget https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-15/SRR8615805/SRR8615805.1;
+wget https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-15/SRR8615804/SRR8615804.1;
+wget https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-15/SRR8616163/SRR8616163.1;
+wget https://sra-download.ncbi.nlm.nih.gov/traces/sra77/SRR/008413/SRR8615465;
+wget https://sra-download.ncbi.nlm.nih.gov/traces/sra77/SRR/008414/SRR8616050;
+wget https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-15/SRR8615561/SRR8615561.1;
+wget https://sra-download.ncbi.nlm.nih.gov/traces/sra77/SRR/008413/SRR8615853;
+wget https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-15/SRR8615538/SRR8615538.1;
+wget https://sra-download.ncbi.nlm.nih.gov/traces/sra77/SRR/008413/SRR8615674;
